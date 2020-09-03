79ed6a9aeff95bf67892fcce83f66444df19ad3f
Pramuka Jayathilaka <opasan4all@gmail.com>
2020-09-03 12:38:58 +0530

    1238hrs 03-08-2020
    1238hrs 03-08-2020

--- form/connect.php
+++ form/connect.php
@@ -14,11 +14,16 @@ if(!empty($username)){
         $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
 
         if(mysqli_connect_error()){
-         die('Connect Error ('.mysqli_connect_errno().')' .mysqli_connect_error());
+         die('Connect Error ('.mysqli_connect_errno() .')' . mysqli_connect_error());
          {
              else{
-                 $sql = "INSERT INTO form (username, password) values ('$username', '$password')";
-                 //if ($conn->query($sql)) 
+                 $sql = "INSERT INTO from (username, password) values ('$username', '$password')";
+                 if ($conn->query($sql)) {
+                     echo "New record is inserted sucessfully";
+                 }
+                 else {
+                     echo "Error: ". $sql . "<br>" . $conn->close();
+                 }
              }
          }   
         }
